c314fcd685568b97a5971b2191ee4512304ed7dd
parabshubham787 <parabshubham787@gmail.com>
2021-12-28 11:14:25 +0530

    by mangesh

---
+++ FirstDemo/FirstDemo/src/main/java/com/csi/service/ServiceApp.java
@@ -0,0 +1,15 @@
+package com.csi.service;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/app")
+public class ServiceApp {
+    @GetMapping()
+    public String SayHello()
+    {
+        return "welcome";
+    }
+}
